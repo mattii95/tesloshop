82548b7f165370245d49e6a4e261185b1f64779a
Matias Courroux <mcourroux@hotmail.com>
2024-12-26 13:40:37 -0300

    config fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { titleFont } from "@/config/fonts";
 
 export default function Home() {
     return (
         <main>
             <h1>Hello world</h1>
+            <h1 className={titleFont.className}>Hello world 2</h1>
         </main>
     );
 }
